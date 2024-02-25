875a6e061099611291d166e4bd14906aea395170
kaifonavt <artur.mikha@gmail.com>
2024-02-25 08:24:10 +0300

    snip init

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Input } from 'react-native-elements';
+import { supabase } from './supabase';
 
-export default function App() {
+export default function EmailPasswordAuth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signInWithEmailPassword = async () => {
+    try {
+      const { user, error } = await supabase.auth.signIn({
+        email,
+        password,
+      });
+      if (error) {
+        console.error('Error signing in with email and password:', error.message);
+      } else {
+        console.log('Signed in with email and password:', user);
+        // Действия после успешной аутентификации
+      }
+    } catch (error) {
+      console.error('Error signing in with email and password:', error.message);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Input
+        placeholder="Email"
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+      <Input
+        placeholder="Password"
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button
+        title="Sign in"
+        onPress={signInWithEmailPassword}
+      />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
