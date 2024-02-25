fe1c6aabca9adb9d6d25a806aa160f5b1b491b06
qrob <Abdraximm@gmail.com>
2024-02-25 12:37:20 +0600

    dolzhno

--- src/initSupabase.ts
+++ src/initSupabase.ts
@@ -1,15 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-<<<<<<< Updated upstream
-// Better put your these secret keys in .env file
-export const supabase = createClient("supabaseUrl", "supabaseKey", {
-=======
 
 
 // Better put your these secret keys in .env file
 export const supabase = createClient("https://edyxrrxfaqktpjggwevz.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeXhycnhmYXFrdHBqZ2d3ZXZ6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwODAxMDE5NSwiZXhwIjoyMDIzNTg2MTk1fQ.kyCIX4M0MZffATuHDcmQXq6Kj7ZG2w97dIVGK7grP3g", {
->>>>>>> Stashed changes
   localStorage: AsyncStorage as any,
   detectSessionInUrl: false // Prevents Supabase from evaluating window.location.href, breaking mobile
 });
